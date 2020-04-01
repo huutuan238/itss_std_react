841949a70611f8ee6cb1db85ac0148aebedb8a58
huutuan238 <huutuan238@gmail.com>
2020-04-01 10:10:04 +0700

    btbn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      toDoList: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <form className="App-form" onSubmit={event => {
+          let toDoList = this.state.toDoList;
+          toDoList.push({
+            title: this.state.title,
+            description: this.state.description
+          });
+          this.setState({toDoList: toDoList, title: "", description: ""});
+          event.preventDefault();
+        }}>
+          <input placeholder="title" value={this.state.title}
+                 onChange={event => this.setState({title: event.target.value})}/>
+          <textarea placeholder="description" value={this.state.description}
+                    onChange={event => this.setState({description: event.target.value})}/>
+          <button>Add</button>
+        </form>
         <div>
-          <ToDoListItem
-          />
+          {
+            this.state.toDoList.map((item, i) => {
+              return <ToDoListItem key={i} title={item.title} description={item.description} onItemClick={() => {
+                let toDoList = this.state.toDoList.filter((item, index) => index !== i);
+                this.setState({toDoList: toDoList});
+              }}/>
+            })
+          }
         </div>
       </div>
     );
--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,10 +3,11 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
+    const {title, description, onItemClick} = this.props;
     return (
-      <div className="ToDoListItem">
-        <div className="ToDoListItem-title"></div>
-        <div className="ToDoListItem-description"></div>
+      <div className="ToDoListItem" onClick={onItemClick}>
+        <div className="ToDoListItem-title">{title}</div>
+        <div className="ToDoListItem-description">{description}</div>
       </div>
     );
   }
